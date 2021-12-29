c81fe8565294aedb14e12c9bde25c09ce8083beb
RajuGupta8019 <94343574+RajuGupta8019@users.noreply.github.com>
2021-12-29 20:41:50 +0530

    Update BasicMathsTests.cs

--- WinAppUnitTest/BasicMathsTests.cs
+++ WinAppUnitTest/BasicMathsTests.cs
@@ -28,12 +28,6 @@ namespace PhApp.Tests
             double res = bm.divide(10, 5);
             Assert.AreEqual(res, 2);
         }
-       // [TestMethod]
-       // public void Test_MultiplyMethod()
-      //  {
-       //     BasicMaths bm = new BasicMaths();
-        //    double res = bm.Multiply(10, 10);
-       //     Assert.AreEqual(res, 100);
-      //  }
+    
     }
 }
